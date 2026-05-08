f4a125ad531d0c563dd6f3a254efd00a370af67c
David <davidfelipecarrillogarzon@gmail.com>
2026-05-08 12:44:16 -0500

    Initial commit: User class structure and registration logic in DavBank

---
+++ src/User.java
@@ -0,0 +1,23 @@
+public class User {
+
+        private int id;
+        private String userName;
+        private String password;
+        private String cardNumber;
+        private String transactionPassword;
+
+        public User(int id, String userName, String password, String cardNumber, String transactionPassword){
+                this.id = id;
+                this.userName = userName;
+                this.password = password;
+                this.cardNumber = cardNumber;
+                this.transactionPassword = transactionPassword;
+        }
+
+        //Getters: help extract data from a private object
+        public int getId(){return id;}
+        public String getUserName(){return userName;}
+        public String getPassword(){return password;}
+        public String getCardNumber(){return  cardNumber;}
+        public String getTransactionPassword(){return transactionPassword;}
+}
