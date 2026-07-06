0d8b0d22e6050331c8f1b8cced3767f79aeb08d1
David <davidfelipecarrillogarzon@gmail.com>
2026-07-06 17:08:59 -0500

    feat: update DBConnection to connect with PostgreSQL

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/davbank_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "TPSOLKN2211";
+
+    public static Connection obtainConnection()throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
