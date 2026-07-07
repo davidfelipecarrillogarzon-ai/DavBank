5f1f2bfccc73810650a46e147244cd8ea2365845
David <davidfelipecarrillogarzon@gmail.com>
2026-07-07 05:22:02 -0500

    fix: balance is added in database users

--- src/UserDAO.java
+++ src/UserDAO.java
@@ -2,10 +2,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 public class UserDAO {
-    public void insert(String userName, String password, String cardNumber, String transactionPin){
+    public void insert(String userName, String password, String cardNumber, String transactionPin, double balance){
         String sql = """
-                INSERT INTO users (userName, password, cardNumber, transactionPin)
-                VALUES(?, ?, ?, ?)
+                INSERT INTO users (userName, password, cardNumber, transactionPin, balance)
+                VALUES(?, ?, ?, ?, ?)
                 """;
 
         try(Connection con = DBConnection.obtainConnection();
@@ -14,6 +14,7 @@ public class UserDAO {
             pstmt.setString(2, password);
             pstmt.setString(3, cardNumber);
             pstmt.setString(4, transactionPin);
+            pstmt.setDouble(5, balance);
 
             pstmt.executeUpdate();
             System.out.println("User has been added in database");
